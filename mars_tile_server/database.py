0da5fd9710af7895f69bb0646d53955172db1e6f
Daven Quinn <dev@davenquinn.com>
2021-11-08 15:38:09 -0600

    Updated test fixtures

--- mars_tile_server/database.py
+++ mars_tile_server/database.py
@@ -2,19 +2,22 @@ from os import environ
 from sparrow.birdbrain import Database
 
 db = None
-try:
-    db = Database(environ.get("FOOTPRINTS_DATABASE"))
-except AttributeError:
-    pass
 
 
-def get_database():
+def get_database(setup=False):
     if db is None:
         return None
-    if getattr(db, "mapper") is None:
+    if setup and getattr(db, "mapper") is None:
         db.automap()
         # We seem to have to remap public for changes to take hold...
         db.mapper.reflect_schema("public")
         db.mapper.reflect_schema("imagery")
         db.mapper.reflect_schema("public")
     return db
+
+
+try:
+    db = Database(environ.get("FOOTPRINTS_DATABASE"))
+    get_database(setup=True)
+except AttributeError:
+    pass
