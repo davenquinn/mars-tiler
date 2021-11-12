7208a47460fa809a5855b6fef49e805245a53c5e
Daven Quinn <dev@davenquinn.com>
2021-11-12 00:22:33 -0600

    Use fork of morecantile

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 all: test
 
+install:
+	poetry install
+	poetry run pip install -r poetry-overrides.txt
+
 test:
-	poetry run pytest --log-cli-level=INFO -x --show-capture=stdout --durations=10
+	poetry run pytest --log-cli-level=INFO --show-capture=stdout --durations=10
--- mars_tile_server/defs/__init__.py
+++ mars_tile_server/defs/__init__.py
@@ -1,11 +1,12 @@
 from morecantile import tms
 from .crs import MARS2000_SPHERE, MARS_MERCATOR
-from .base import MarsTMS
+from morecantile.models import TileMatrixSet
 
 mercator_tms = tms.get("WebMercatorQuad")
-mars_tms = MarsTMS.custom(
+mars_tms = TileMatrixSet.custom(
     mercator_tms.bbox,
     MARS_MERCATOR,
     extent_crs=MARS2000_SPHERE,
     title="Web Mercator Mars",
+    geographic_crs=MARS2000_SPHERE,
 )
