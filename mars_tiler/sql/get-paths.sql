aff40e750a313ad44d975997cca2fda7c564cc8a
Daven Quinn <dev@davenquinn.com>
2021-12-31 17:55:02 -0600

    Fallback to mosaic parameters

--- mars_tiler/sql/get-paths.sql
+++ mars_tiler/sql/get-paths.sql
@@ -1,10 +1,12 @@
 SELECT
   "path",
-  minzoom,
-  maxzoom,
-  rescale_range,
-  mosaic
-FROM imagery.dataset
+  coalesce(d.minzoom, m.minzoom) minzoom,
+  coalesce(d.maxzoom, m.maxzoom) maxzoom,
+  coalesce(d.rescale_range, m.rescale_range) rescale_range,
+  d.mosaic
+FROM imagery.dataset d
+JOIN imagery.mosaic m
+  ON d.mosaic = m.name
 WHERE ST_Intersects(
     footprint,
     ST_SetSRID(ST_MakeEnvelope(:x1,:y1,:x2,:y2), 949900)
